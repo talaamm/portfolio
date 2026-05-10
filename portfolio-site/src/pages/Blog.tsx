c15e4c782114f0250618b77d742608da0cc4d221
Tala Amm <150236890+talaamm@users.noreply.github.com>
2026-05-10 07:56:41 +0300

    Update Blog.tsx

--- portfolio-site/src/pages/Blog.tsx
+++ portfolio-site/src/pages/Blog.tsx
@@ -87,7 +87,7 @@ const Blog = () => {
   const featuredPosts = sortedPosts.slice(0, 3);
   const regularPosts = sortedPosts.slice(3);
 
-  const totalViewsComputed = backendTotalViews ?? 4700 ;
+  const totalViewsComputed = backendTotalViews ?? 7800 ;
 
   const stats = {
     totalPosts: blogPosts.length,
